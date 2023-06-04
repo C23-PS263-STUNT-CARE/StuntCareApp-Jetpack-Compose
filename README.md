060700433d2c9acaa0ba8e6d81dadfeaff2c68fb
febiarifin <febiarifin0@gmail.com>
2023-06-04 19:25:03 +0700

    Update CheckScreen and README.md

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 <table>
    <tr>
       <td><img src="screenshots/home.jpeg" align="center" alt="Home"></td>
+      <td><img src="screenshots/education.jpeg" align="center" alt="Education"></td>
+   </tr> 
+    <tr>
+      <td><img src="screenshots/check.jpeg" align="center" alt="Check"></td>
+      <td><img src="screenshots/check_empty.jpeg" align="center" alt="Check Empty"></td>
    </tr> 
 </table>
--- app/src/main/java/com/febiarifin/stuntcare/ui/screen/check/CheckScreen.kt
+++ app/src/main/java/com/febiarifin/stuntcare/ui/screen/check/CheckScreen.kt
@@ -1,14 +1,21 @@
 import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -16,21 +23,29 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.febiarifin.stuntcare.R
 import com.febiarifin.stuntcare.model.Article
 import com.febiarifin.stuntcare.model.Check
 import com.febiarifin.stuntcare.model.dummyArticle
@@ -67,7 +82,7 @@ fun CheckScreen() {
             )
         },
     ) {
-        Column{
+        Column {
             Column {
                 Spacer(modifier = Modifier.height(60.dp))
                 CheckList(dummyCheck)
@@ -81,16 +96,70 @@ fun CheckList(
     listCheck: List<Check>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = modifier.padding(0.dp,0.dp,0.dp,80.dp)
-    ) {
-        items(listCheck, key = { it.id }) { check ->
-            CheckItem(
-                check,
-                modifier = Modifier.clickable{}
-            )
+    if (listCheck.isEmpty()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(210.dp)
+                    .width(300.dp)
+                    .padding(4.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.White)
+                    .border(
+                        1.dp,
+                        shape = RoundedCornerShape(16.dp),
+                        color = Color.Gray.copy(alpha = 0.3f)
+                    ),
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .size(90.dp),
+                        painter = painterResource(R.drawable.banner_header),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = modifier.height(8.dp))
+                    Text(
+                        fontSize = 14.sp,
+                        text = stringResource(R.string.jargon_1),
+                        color = Color.Gray
+                    )
+                    Spacer(modifier = modifier.height(8.dp))
+                    Button(
+                        onClick = { /*TODO*/ },
+                        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary)
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_add),
+                            contentDescription = null
+                        )
+                        Spacer(modifier = modifier.width(6.dp))
+                        Text("Cek Status Stunting")
+                    }
+                }
+            }
+        }
+    } else {
+        LazyColumn(
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = modifier.padding(0.dp, 0.dp, 0.dp, 80.dp)
+        ) {
+            items(listCheck, key = { it.id }) { check ->
+                CheckItem(
+                    check,
+                    modifier = Modifier.clickable {}
+                )
+            }
         }
     }
 }
